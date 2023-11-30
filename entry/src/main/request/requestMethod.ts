059c852952eb29ddf2287cd4ecd0eaaa5716a3e6
vagmr <2737165206@qq.com>
2023-11-30 21:15:26 +0800

    feat:一些添加

--- entry/src/main/request/requestMethod.ts
+++ entry/src/main/request/requestMethod.ts
@@ -1,4 +1,4 @@
-import { requestInstance } from "../../main/request/comRequest"
+import { requestInstance } from "./comRequest"
 export  const getMethod = async () => {
     const res:any = await requestInstance(
         {url:"http://localhost:3000/api/quotes"
