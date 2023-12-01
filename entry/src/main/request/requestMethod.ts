f475f767fb3709249b118dd65b42bc666d8cdc0b
vagmr <2737165206@qq.com>
2023-12-01 17:26:25 +0800

    docs:修改函数名，使语义化

--- entry/src/main/request/requestMethod.ts
+++
@@ -1,7 +0,0 @@
-import { requestInstance } from "./comRequest"
-export  const getMethod = async () => {
-    const res:any = await requestInstance(
-        {url:"http://localhost:3000/api/quotes"
-        })
-console.log(res)
-}
