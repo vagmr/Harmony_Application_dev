b55052c0d51f2b3fda7581a1cc656ced3bfe32f0
vagmr <2737165206@qq.com>
2023-12-04 00:22:28 +0800

    feat:add new pages

--- entry/src/main/ets/type/comon_res.d.ts
+++ entry/src/main/ets/type/comon_res.d.ts
@@ -1,9 +1,15 @@
+//标准返回模式
 export  interface Data<T> {
   code: string
   msg: string
   data: T
 }
+//返回值类型
 export  interface sayingType{
   id:number
+  content:string
+}
+export  interface  wordType{
+  code:number
   content:string
 }
--- entry/src/main/request/methodApi.ts
+++ entry/src/main/request/methodApi.ts
@@ -5,3 +5,7 @@ export  const getSaying = () => {
     return requestInstance(
       {url:"https://fakeapi.vagmrgpt.top/saying" })
 }
+export const getLoveWord = () => {
+  return requestInstance
+  ({url:"https://fakeapi.vagmrgpt.top/wordlist"})
+}
